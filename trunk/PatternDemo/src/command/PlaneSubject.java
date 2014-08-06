e7eaa913076fb15e8a4c2135bc8f49370c26087f
anhtuanluu36@gmail.com <anhtuanluu36@gmail.com@5228155a-d30d-35d9-6b5e-78eabb538e2f>
2014-08-06 10:44:50 +0000

    Implemented the Command pattern

---
+++ trunk/PatternDemo/src/command/PlaneSubject.java
@@ -0,0 +1,16 @@
+package command;
+
+public class PlaneSubject implements Subject {
+
+	@Override
+	public String turnOn() {
+		return "Plane turn on!";
+
+	}
+
+	@Override
+	public String turnOff() {
+		return "Plane turn on!";
+	}
+
+}
